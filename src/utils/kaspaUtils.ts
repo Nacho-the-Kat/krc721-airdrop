8c40e5e0008cdd8e35a038e143811b105eb76c64
Ashton <ashton@onargon.com>
2025-04-02 23:02:14 -0700

    clean

--- src/utils/kaspaUtils.ts
+++
@@ -1,7 +0,0 @@
-import { PrivateKey } from "../../wasm/kaspa";
-
-export function createPrivateKeyFromString(privateKeyStr: string): PrivateKey {
-    // TODO: Update this implementation based on WASM documentation
-    // For now, we'll throw an error to indicate this needs to be implemented
-    throw new Error("Private key creation method needs to be implemented based on WASM documentation");
-} 
